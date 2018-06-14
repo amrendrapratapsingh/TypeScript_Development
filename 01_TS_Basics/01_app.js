ae264eff9d4b716cfd2d60be9d31574b7fd581b9
Amrendra Pratap Singh <amrendrapratapcoders@gmail.com>
2018-06-15 00:16:05 +0530

    01_TS_Basics

--- 01_TS_Basics/01_app.js
+++ 01_TS_Basics/01_app.js
@@ -1,12 +1,12 @@
 // Hello World App
-var greeting = "Gud morng Ts";
+var greeting = "Good Morning TS";
 console.log(greeting);
 document.getElementById('display').innerHTML = greeting;
 // Variables Creation in TypeScript
 function greet(name) {
-    var greeting = "good morng" + name;
+    var greeting = "Good Morning " + name;
     return greeting;
 }
-var output = greet('john');
+var output = greet('John');
 console.log(output);
 document.getElementById('display').innerHTML = output;
--- 01_TS_Basics/01_app.ts
+++ 01_TS_Basics/01_app.ts
@@ -1,19 +1,19 @@
 // Hello World App
 
-let  greeting:string = "Gud morng Ts";
-console.log(greeting);
-document.getElementById('display').innerHTML = greeting;
+    let greeting:string = "Good Morning TS";
+    console.log(greeting);
+    document.getElementById('display').innerHTML = greeting;
+
 
 // Variables Creation in TypeScript
 
- function greet(name:string):string{
+    function greet(name:string):string{
+        let greeting:string = "Good Morning " + name;
+        return greeting;
+    }
 
-    let greeting : string ="good morng" + name;
-    return greeting;
-}
-let output : string = greet('john');
-console.log(output);
-document.getElementById('display').innerHTML = output;
-
+    let output :string = greet('John');
+    console.log(output);
+    document.getElementById('display').innerHTML = output;
 
 
